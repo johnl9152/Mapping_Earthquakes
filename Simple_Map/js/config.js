cd5918832677962524e09fba440e51e604d70e7a
johnl9152 <johnl9152@gmail.com>
2022-01-24 18:15:56 -0600

    13.4.3

--- Simple_Map/js/config.js
+++
@@ -1,2 +0,0 @@
-// API Key
-const API_KEY = "pk.eyJ1Ijoiam9obmw5MTUyIiwiYSI6ImNreW5heWt6NDJ2N2EzM3FobDRzbWdhdnEifQ.Bws6evnnmISHH8BdtHmxUw";
--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -4,12 +4,8 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map("mapid").setView([40.7,-94.5],4);
 
-//  Add a marker to the map for Los Angeles, California.
-L.circle([34.0522, -118.2437], {
-    radius: 300
-    color: "black"
-    fillColor: '#ffffa1'
- }).addTo(map);
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([41.1977, -97.4231], 4);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -17,5 +13,6 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
     maxZoom: 18,
     accessToken: API_KEY
 });
+
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
